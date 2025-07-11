a09435cc95375dc35247efa07993323a88a91451
Cwarren15-A <warrencameron15@outlook.com>
2025-07-11 16:36:01 -0700

    Simplify App component for initial deployment

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,49 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import { useIsAuthenticated } from '@azure/msal-react'
-import Layout from './components/Layout'
 import LoginPage from './pages/LoginPage'
-import DashboardPage from './pages/DashboardPage'
-import ContactsPage from './pages/ContactsPage'
-import ContactDetailPage from './pages/ContactDetailPage'
-import InteractionsPage from './pages/InteractionsPage'
-import NotesPage from './pages/NotesPage'
-import TasksPage from './pages/TasksPage'
-import SettingsPage from './pages/SettingsPage'
 
 function App() {
-  const isAuthenticated = useIsAuthenticated()
-
-  if (!isAuthenticated) {
-    return <LoginPage />
-  }
-
   return (
-    <Router>
-      <div className="App">
-        <Layout>
-          <Routes>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route path="/contacts/:id" element={<ContactDetailPage />} />
-            <Route path="/interactions" element={<InteractionsPage />} />
-            <Route path="/notes" element={<NotesPage />} />
-            <Route path="/tasks" element={<TasksPage />} />
-            <Route path="/settings" element={<SettingsPage />} />
-          </Routes>
-        </Layout>
-        <Toaster 
-          position="top-right"
-          toastOptions={{
-            duration: 4000,
-            style: {
-              background: '#363636',
-              color: '#fff',
-            },
-          }}
-        />
-      </div>
-    </Router>
+    <div className="App">
+      <LoginPage />
+    </div>
   )
 }
 
